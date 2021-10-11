55b94b36b1e4b64f4b91bdd559a0ec6f9e670667
Siddeshbangera <75700085+Siddeshbangera@users.noreply.github.com>
2021-10-11 21:26:14 +0100

    update cred

--- dbms/db.config.js
+++ dbms/db.config.js
@@ -2,6 +2,6 @@ module.exports = {
     HOST: "database-1.clficyiqbfmd.us-east-2.rds.amazonaws.com",
     USER: "root",
     PORT:"3306",
-    PASSWORD: "12345678",
+    PASSWORD: "YakZebra123__",
     DB: "cms"
   };
