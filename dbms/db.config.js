9e2e8ac3cf12e6c2bc0581099effb7973d2eb7c0
Siddeshbangera <75700085+Siddeshbangera@users.noreply.github.com>
2022-01-18 21:03:02 +0000

    Update db.config.js

--- dbms/db.config.js
+++ dbms/db.config.js
@@ -2,6 +2,6 @@ module.exports = {
     HOST: "database-1.clficyiqbfmd.us-east-2.rds.amazonaws.com",
     USER: "root",
     PORT:"3306",
-    PASSWORD: "YakZebra123__",
+    PASSWORD: "Sidhu123",
     DB: "cms"
   };
